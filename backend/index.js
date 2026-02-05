c863a96ab3cc58ec4747aee3e78c74c48695d933
Atul Kumar Jha <atuljha21542@gmail.com>
2026-02-05 23:37:14 +0530

    fixed PORT issue

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(express.json());
 
 const mongoUrl = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/heartbeat";
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
